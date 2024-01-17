e72dff8baf237422c04541b1f4f3b709eff6f739
tmlaDesign <tmlaDesign@gmail.com>
2024-01-17 10:41:13 -0500

    Listing all students on page - next step is to reduce to 9 on single page.

--- js/script.js
+++ js/script.js
@@ -17,10 +17,12 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-const main
 
-function showPage(arr) {
-let students = ''
+const studentList = document.querySelector('ul');
+
+
+function showStudents(arr) {
+   let students = '';
       for (let i = 0 ; i < arr.length; i++) {
          students += `<li class="student-item cf">
          <div class="student-details">
@@ -31,14 +33,19 @@ let students = ''
          <div class="joined-details">
          <span class="date">${arr[i].registered.date}</span>
          </div>
-         </li>`
-   }
+         </li>`;
+      }
    return students;
-     
+
 }
 
-console.log(showPage(data));
 
+
+
+
+console.log(showStudents(data));
+
+studentList.insertAdjacentHTML('beforeend', showStudents(data));
 /*
 Create the addPagination function
 This function will create and insert/append the elements needed for the pagination buttons
