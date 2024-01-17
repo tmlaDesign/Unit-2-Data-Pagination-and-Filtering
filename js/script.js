42fbd0f356637b32cd052591e31987735bf09fca
tmlaDesign <tmlaDesign@gmail.com>
2024-01-17 09:49:33 -0500

    Added showPage function to create each li containing student details.

--- js/script.js
+++ js/script.js
@@ -17,11 +17,30 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const main
 
+function showPage(arr) {
+let students = ''
+      for (let i = 0 ; i < arr.length; i++) {
+         students += `<li class="student-item cf">
+         <div class="student-details">
+         <img class="avatar" src="${arr[i].picture.large}" alt="${arr[i].name.first}">
+         <h3>${arr[i].name.first}</h3>
+         <span class="email">${arr[i].email}</span>
+         </div>
+         <div class="joined-details">
+         <span class="date">${arr[i].registered.date}</span>
+         </div>
+         </li>`
+   }
+   return students;
+     
+}
 
+console.log(showPage(data));
 
 /*
-Create the `addPagination` function
+Create the addPagination function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
