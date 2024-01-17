f24eeb6aa0b35baf33c382cd4e4bfc693f94a53f
tmlaDesign <tmlaDesign@gmail.com>
2024-01-17 11:06:43 -0500

    Adjusted code to only show 9 students per page

--- js/script.js
+++ js/script.js
@@ -18,34 +18,60 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+
+
+ // create two variables that will represent the index for the first and last student on the page
+
+  // select the element with a class of `student-list` and assign it to a variable
+
+  // set the innerHTML property of the variable you just created to an empty string
+
+  // loop over the length of the `list` parameter
+    // inside the loop create a conditional to display the proper students
+      // inside the conditional:
+        // create the elements needed to display the student information
+        // insert the above elements
+
 const studentList = document.querySelector('ul');
+studentList.innerHTML = '';
 
+function showPage(list, page) {
+   
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9
 
-function showStudents(arr) {
-   let students = '';
-      for (let i = 0 ; i < arr.length; i++) {
-         students += `<li class="student-item cf">
-         <div class="student-details">
-         <img class="avatar" src="${arr[i].picture.large}" alt="${arr[i].name.first}">
-         <h3>${arr[i].name.first}</h3>
-         <span class="email">${arr[i].email}</span>
-         </div>
-         <div class="joined-details">
-         <span class="date">${arr[i].registered.date}</span>
-         </div>
-         </li>`;
+   console.log(list);
+   console.log(page);
+   console.log(startIndex);
+   console.log(endIndex);
+   
+   let studentItem = '';
+      for (let i = 0 ; i < list.length; i++) {
+
+         if (i >= startIndex && i < endIndex) {
+            studentItem += `<li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="${list[i].name.first}">
+            <h3>${list[i].name.first}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date">${list[i].registered.date}</span>
+            </div>
+            </li>`;
+
+         }
+        
       }
-   return students;
-
+      
+   return studentItem;
+   
 }
 
 
 
-
-
-console.log(showStudents(data));
-
-studentList.insertAdjacentHTML('beforeend', showStudents(data));
+console.log(studentList);
+studentList.insertAdjacentHTML('beforeend', showPage(data, 1));
 /*
 Create the addPagination function
 This function will create and insert/append the elements needed for the pagination buttons
